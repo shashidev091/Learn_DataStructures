cb0bc162963e07274b175704dabd117968f06015
Shashi Bhagat <skujur871@gmail.com>
2021-09-12 22:12:52 +0530

    Arrays DataStructure

---
+++ src/com/arrays/Arrayss.java
@@ -0,0 +1,63 @@
+package com.arrays;
+
+import java.util.Arrays;
+
+public class Arrayss extends CustomArray {
+    int[] numbers = {1, 2, 3, 4, 5};
+
+    public Arrayss(int length) {
+        super(length);
+//        System.out.println(Arrays.toString(numbers));
+    }
+
+    // insert array using its methods
+    public void insertAndPrint() {
+
+    }
+}
+
+class CustomArray {
+    private int[] items;
+    private int count = 0;
+
+    public CustomArray(int length) {
+        items = new int[length];
+    }
+
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i]);
+        }
+    }
+
+    public void insert(int item) {
+        if (items.length == count) {
+            int[] newItems = new int[count * 2];
+
+            for (int i = 0; i < count; i++)
+                newItems[i] = items[i];
+
+            items = newItems;
+        }
+        items[count++] = item;
+    }
+
+    public void removeAt(int index) {
+        if (index < 0 || index >= count) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = index; i < count; i++) {
+            items[i] = items[i + 1];
+        }
+
+        count--;
+    }
+
+    public int indexOf(int item) {
+        for (int i = 0; i < items.length; i++ )
+            if (items[i] == item)
+                return i;
+        return -1;
+    }
+}
--- src/com/shashi/Main.java
+++ src/com/shashi/Main.java
@@ -1,8 +1,25 @@
 package com.shashi;
 
+import com.arrays.Arrayss;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Arrayss array = new Arrayss(4);
+        array.print();
+
+        array.insert(1);
+        array.insert(5);
+        array.insert(2);
+
+        array.print();
+        System.out.println();
+        array.removeAt(2);
+        array.print();
+        int a = array.indexOf(5);
+        System.out.println(a);
+
+
     }
 }
